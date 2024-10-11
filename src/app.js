1af181aca7912469bbb0a5d6a9a89b8449b675cd
Maikel Contramaestre <116273585+BlackNereus@users.noreply.github.com>
2024-10-11 08:44:56 +0000

    FS_PT_81_excuseGenerator_MaikelContramaestre

--- src/app.js
+++ src/app.js
@@ -4,8 +4,31 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { arrow } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const genRandom = arr => {
+    return Math.floor(Math.random() * arr.length);
+  };
+
+  console.log(
+    who[genRandom(who)] +
+      " " +
+      action[genRandom(action)] +
+      " " +
+      what[genRandom(what)] +
+      " " +
+      when[genRandom(when)]
+  );
 };
